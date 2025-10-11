5a2a6a59e196abca00e93703356fb977791fd62f
starbook <JinAdonis@outlook.com>
2025-10-11 09:21:57 +0800

    feat(usart): implement USART DMA double buffer initialization

--- code/bsp/usart/bsp_usart.c
+++ code/bsp/usart/bsp_usart.c
@@ -2,81 +2,18 @@
 
 #include "plf_log.h"
 
-// static int uart_receive_dma_no_it(UART_HandleTypeDef *huart, uint8_t *pData, uint32_t Size)
-// {
-//     uint32_t tmp = 0;
-//     tmp = huart->RxState;
-//
-//     /* ÅÐ¶Ï´®¿ÚÊÇ·ñÒÑ¾­³õÊ¼»¯Íê³É */
-//     if (tmp == HAL_UART_STATE_READY)
-//     {
-//         /* ¼ì²âÓÃ»§ÊäÈëµÄÊý¾ÝÊÇ·ñÕýÈ· */
-//         if ((pData == NULL) || (Size == 0))
-//             return HAL_ERROR;
-//
-//         huart->pRxBuffPtr = pData;
-//         huart->RxXferSize = Size;
-//         huart->ErrorCode = HAL_UART_ERROR_NONE;
-//
-//         /* Ê¹ÄÜDMAÍ¨µÀ */
-//         HAL_DMA_Start(huart->hdmarx, (uint32_t)&huart->Instance->RDR, (uint32_t)pData, Size);
-//
-//         /* ¿ªÆôDMA´«Êä ½«UART CR3 ¼Ä´æÆ÷ÖÐµÄ DMARÎ» ÖÃ¸ß */
-//         SET_BIT(huart->Instance->CR3, USART_CR3_DMAR);
-//
-//         return HAL_OK;
-//     }
-//     else
-//         return HAL_BUSY;
-// }、
+ void USART_RxDMA_DoubleBuffer_Init(UART_HandleTypeDef *huart, uint32_t *DstAddress, uint32_t *SecondMemAddress, uint32_t DataLength){
 
-/**
- *
- * @param usartInstance 串口实例指针
- * @return true 开启 对应串口DMA | false 开启失败
- */
-static bool Uart_Receive_DMA_NO_IT(UartInstance_s *usartInstance)
-{
-    uint32_t tmp = 0;
-    tmp = usartInstance->huart_handle->RxState;
-    if (tmp == HAL_UART_STATE_READY)
-    {
-        if (usartInstance->ring_buff.data == NULL || usartInstance->ring_buff.element_size == 0) {
-            Log_Error("%s ring_buff or rx_len is NULL");
-            return false;
-        }
-        usartInstance->huart_handle->pRxBuffPtr = usartInstance->ring_buff.data;
-        usartInstance->huart_handle->RxXferSize = usartInstance->ring_buff.element_size;
-        usartInstance->huart_handle->ErrorCode = HAL_UART_ERROR_NONE;
+    huart->ReceptionType = HAL_UART_RECEPTION_TOIDLE;
 
-        HAL_DMA_Start(usartInstance->huart_handle->hdmarx,(uint32_t)&usartInstance->huart_handle->Instance->RDR,
-            (uint32_t)usartInstance->ring_buff.data,usartInstance->ring_buff.element_size);
+    huart->RxEventType = HAL_UART_RXEVENT_IDLE;
 
-        SET_BIT(usartInstance->huart_handle->Instance->CR3,USART_CR3_DMAR);
-        return true;
-    }
-        return false;
+    huart->RxXferSize    = DataLength;
+
+    SET_BIT(huart->Instance->CR3,USART_CR3_DMAR);
+
+    __HAL_UART_ENABLE_IT(huart, UART_IT_IDLE);
+
+    HAL_DMAEx_MultiBufferStart(huart->hdmarx,(uint32_t)&huart->Instance->RDR,(uint32_t)DstAddress,(uint32_t)SecondMemAddress,DataLength);
 }
-// /**
-//   * @brief	¿ÕÏÐÖÐ¶Ï³õÊ¼»¯º¯Êý
-//   * @param	huart:UART¾ä±úÖ¸Õë
-//   * @retval	none
-//   */
-// void uart_receive_init(UART_HandleTypeDef *huart)
-// {
-//     if (huart == &huart5)
-//     {
-//         /* Çå³ý¿ÕÏÐÖÐ¶Ï±êÖ¾Î» */
-//         __HAL_UART_CLEAR_IDLEFLAG(&huart5);
-//         /* ¿ªÆô´®¿Ú¿ÕÏÐÖÐ¶Ï */
-//         __HAL_UART_ENABLE_IT(&huart5, UART_IT_IDLE);
-//         /* ¿ªÆôDMA½ÓÊÕ Ö¸¶¨½ÓÊÕ³¤¶ÈºÍÊý¾ÝµØÖ· */
-//         uart_receive_dma_no_it(&huart5, usart5_buf, USART5_MAX_LEN);
-//     }
-// }
 
-static void Uart_Receive_Init(UartInstance_s *usartInstance) {
-    __HAL_UART_CLEAR_IDLEFLAG(usartInstance->huart_handle);
-    __HAL_UART_ENABLE_IT(usartInstance->huart_handle, UART_IT_IDLE);
-
-}
--- code/bsp/usart/bsp_usart.h
+++ code/bsp/usart/bsp_usart.h
@@ -3,13 +3,13 @@
 #include "bsp_typedef.h"
 #include <stdint.h>
 #include "usart.h"
-#include  "circular_queue.h"
 typedef struct UsartInstance_s {
     char *topic_name;                                           // 实例名称
     TransferMode_e mode;                                        // 传输模式
     UART_HandleTypeDef* huart_handle;
+    uint8_t* first_rx_buf;                                 // 第一个接收缓冲区
+    uint8_t* second_rx_buf;                                // 第二个接收缓冲区
     DirectionMode_e direction;                                  // 传输方向
-    CircularQueue_s ring_buff;
     uint8_t tx_len;                                             // 发送数据长度
     uint8_t rx_len;                                             // 接收数据长度
     void (*usart_module_callback)(struct UsartInstance_s *);    // 回调函数
@@ -26,4 +26,7 @@ typedef struct {
     void (*usart_module_callback)(struct UsartInstance_s *);    // 回调函数
     void *parent_ptr;                                           // 使用USART外设的父模块指针
 } UartInitConfig_s;
+
+void USART_RxDMA_DoubleBuffer_Init(UART_HandleTypeDef *huart, uint32_t *DstAddress, uint32_t *SecondMemAddress, uint32_t DataLength);
+
 #endif
