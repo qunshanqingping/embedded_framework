5966d3cb91bca953a17c988c5da3e915eef1aca8
starbook <JinAdonis@outlook.com>
2025-10-16 10:19:59 +0800

    feat(app_init): add initialization functions and header for command handling

---
+++ code/app/init/app_init.c
@@ -0,0 +1,10 @@
+//
+// Created by 29568 on 2025/10/15.
+//
+
+#include "app_init.h"
+#include "cmd_fun.h"
+
+void sum_init(void){
+     USER_CMD_Init();
+}
---
+++ code/app/init/app_init.h
@@ -0,0 +1,9 @@
+//
+// Created by 29568 on 2025/10/15.
+//
+
+#ifndef APP_INIT_H
+#define APP_INIT_H
+
+void sum_init(void);
+#endif //STEER_WHEEL_HERO_GIMBAL_APP_INIT_H
