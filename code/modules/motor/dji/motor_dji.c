fd18ec9c62b5bab04aaf1c6ee71f8d5b71bedac8
Adonis_Jin <JinAdonis@outlook.com>
2025-10-23 16:03:03 +0800

    feat(motor): implement temperature protection and enhance motor decoding functions

--- code/modules/motor/dji/motor_dji.c
+++ code/modules/motor/dji/motor_dji.c
@@ -1,22 +1,70 @@
 #include "motor_dji.h"
+
+#include "basic_math.h"
 #include "bsp_can.h"
 #include "bsp_fdcan.h"
-
+/**
+ * @file motor_dji.c
+ * @brief 大疆电机温度保护
+ * @param motor_instance 电机实例指针
+ * @date 2025-08-18
+ */
+static void Motor_Temp_Protect(DjiMotorInstance_s *motor_instance) {
+if (motor_instance->message.raw.rotor_temp >= DJI_MOTOR_TEMPERATURE_LIMIT){
+    motor_instance->message.status.temp_overheat_cnt++;
+}
+}
 // ReSharper disable once CppParameterMayBeConstPtrOrRef
-static void MOTOR_Dji_Decode( CanInstance_s *can_instance) {
+static void MOTOR_Dji_M3508_Decode( CanInstance_s *can_instance) {
     if (can_instance == NULL) {
         return;
     }
     DjiMotorInstance_s *motor_instance = can_instance->parent_ptr;
-    motor_instance->message.rotor_angle          // 转子机械角度
+    motor_instance->message.raw.rotor_angle          // 转子机械角度
             = (uint16_t) (can_instance->rx_buff[0] << 8 | can_instance->rx_buff[1]);
-    motor_instance->message.rotor_speed    // 转子转速
+    motor_instance->message.raw.rotor_speed    // 转子转速
             = (uint16_t) (can_instance->rx_buff[2] << 8 | can_instance->rx_buff[3]);
-    motor_instance->message.torque_current // 实际转矩电流
+    motor_instance->message.raw.torque_current // 实际转矩电流
             = (uint16_t) (can_instance->rx_buff[4] << 8 | can_instance->rx_buff[5]);
-    motor_instance->message.rotor_temp   // 电机温度
+    motor_instance->message.raw.rotor_temp   // 电机温度
             = can_instance->rx_buff[6];
-    motor_instance.message.rx_cnt++;
+    motor_instance->message.status.rx_cnt++;
+    Motor_Temp_Protect(motor_instance);
+    motor_instance->message.calc.velocity = motor_instance->message.raw.rotor_speed /motor_instance->reduction_ratio * RPM_TO_RADS; // 计算输出轴角度
+    motor_instance->message.calc.torque = map(motor_instance->message.raw.torque_current,DJI_RAW_TORQUE_CURRENT_MIN, DJI_RAW_TORQUE_CURRENT_MAX,
+                                                DJI_M3508_ACTUAL_TORQUE_CURRENT_MIN, DJI_M3508_ACTUAL_TORQUE_CURRENT_MAX) * motor_instance->torque_constant; // 计算输出轴转矩
+}
+static void DJI_GM6020_Decode( CanInstance_s *can_instance) {
+    if (can_instance == NULL) {
+        return;
+    }
+    DjiMotorInstance_s *motor_instance = can_instance->parent_ptr;
+    motor_instance->message.raw.rotor_angle          // 转子机械角度
+            = (uint16_t) (can_instance->rx_buff[0] << 8 | can_instance->rx_buff[1]);
+    motor_instance->message.raw.rotor_speed    // 转子转速
+            = (uint16_t) (can_instance->rx_buff[2] << 8 | can_instance->rx_buff[3]);
+    motor_instance->message.raw.torque_current // 实际转矩电流
+            = (uint16_t) (can_instance->rx_buff[4] << 8 | can_instance->rx_buff[5]);
+    motor_instance->message.raw.rotor_temp   // 电机温度
+            = can_instance->rx_buff[6];
+    motor_instance->message.status.rx_cnt++;
+    Motor_Temp_Protect(motor_instance);
+    motor_instance->message.calc.velocity = motor_instance->message.raw.rotor_speed /motor_instance->reduction_ratio * RPM_TO_RADS; // 计算输出轴角度
+    motor_instance->message.calc.torque = map(motor_instance->message.raw.torque_current,DJI_RAW_TORQUE_CURRENT_MIN, DJI_RAW_TORQUE_CURRENT_MAX,
+                                                DJI_GM6020_ACTUAL_TORQUE_CURRENT_MIN, DJI_GM6020_ACTUAL_TORQUE_CURRENT_MAX) * motor_instance->torque_constant; // 计算输出轴转矩
+}
+/**
+ * @brief 选择大疆电机解码函数
+ * @param config 大疆电机初始化配置结构体指针
+ */
+static void Motor_Dji_SLect_Decode(DjiMotorInitConfig_s *config){
+    if (config == NULL) {
+        Log_Error("Motor_Dji_SLect_Decode : Config is NULL");
+        return;
+    }
+    if (config->type == M3508){
+        config->can_config.can_module_callback = MOTOR_Dji_M3508_Decode;
+    }
 }
 /**
  * @brief 配置CAN模块
@@ -43,6 +91,6 @@ static void MOTOR_Dji_Can_Set(DjiMotorInitConfig_s *config) {
             config->can_config.tx_id = M_CTRL_5_TO_8_ID;
         }
     }
-    config->can_config.can_module_callback = MOTOR_Dji_Decode;
+    Motor_Dji_SLect_Decode(config);
 }
 
--- code/modules/motor/dji/motor_dji.h
+++ code/modules/motor/dji/motor_dji.h
@@ -9,6 +9,15 @@
 #include "bsp_fdcan.h"
 
 /* 私有类型定义 -----------------------------------------------------------------*/
+#define DJI_RAW_TORQUE_CURRENT_MAX  16384
+#define DJI_RAW_TORQUE_CURRENT_MIN -16384
+#define DJI_M3508_ACTUAL_TORQUE_CURRENT_MAX  20
+#define DJI_M3508_ACTUAL_TORQUE_CURRENT_MIN -20
+#define DJI_GM6020_ACTUAL_TORQUE_CURRENT_MAX  3
+#define DJI_GM6020_ACTUAL_TORQUE_CURRENT_MIN -3
+#define DJI_3508_TORQUE_CONSTANT 15.6223893065998       //M3508转矩常数mN*m/A
+#define DJI_GM6020_TORQUE_CONSTANT 741.0F               //GM6020转矩常数mN*m/A
+#define DJI_MOTOR_TEMPERATURE_LIMIT 80 //热温度限制
 /**
  * @brief 大疆电机类型枚举
  */
@@ -42,25 +51,34 @@ typedef enum {
     DJI_MOTOR_TX_ERROR = 3,   //电机发送异常
     DJI_MOTOR_OVERHEAT = 4,   //电机过热
 } Dji_Motor_State_e;
-
-typedef struct {
-    /* 原始反馈数据 */
+typedef struct{
     uint16_t rotor_angle;     //转子角度
     int16_t rotor_speed;      //转子速度
     int16_t torque_current;   //转矩电流
     uint8_t rotor_temp;       //电机温度
-    /* 计算得到的数据 */
-    float out_position;                 // 电机输出轴角度(-PI~PI)
-    float out_velocity;                 // 电机输出轴速度(rpm)
-    int16_t out_current;        //
+} DJI_Motor_RawMessage_s;
+
+typedef struct{
+    float position;                 // 电机输出轴角度(-PI~PI)
+    float velocity;                 // 电机输出轴速度(rpm)
+    int16_t torque;                  //
     int8_t rotor_rounds;        //转子圈数
-    int16_t out_rounds;         //电机输出轴圈数
-    /* 状态数据 */
+    int16_t rounds;         //电机输出轴圈数
+}DJI_Motor_CalcMessage_s;
+typedef struct{
     uint16_t rx_cnt;            //can接收计数
     uint16_t rx_freq;           //can接受频率
     uint16_t tx_cnt;            //can发送计数
     uint16_t tx_freq;           //can发送频率
     uint16_t temp_overheat_cnt; //过热计数
+}DJI_Motor_Status_s;
+typedef struct {
+    /* 原始反馈数据 */
+    DJI_Motor_RawMessage_s raw;
+    /* 计算得到的数据 */
+    DJI_Motor_CalcMessage_s calc;
+    /* 状态数据 */
+    DJI_Motor_Status_s status;
 } DJI_Motor_message_s;
 
 typedef struct {
@@ -77,6 +95,8 @@ typedef struct DjiMotorInstance_s {
     uint8_t id;
 
     Dji_Motor_State_e state;
+    uint8_t reduction_ratio;              // 减速比
+    float torque_constant;                // 转矩常数 mN*m/A
     DJI_Motor_message_s message;
 
     CanInstance_s *can_instance;
